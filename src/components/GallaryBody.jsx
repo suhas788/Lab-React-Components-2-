5043c6b72a54c10d47d44d8aa96567964662e975
suhas788 <141901404+suhas788@users.noreply.github.com>
2024-02-09 15:58:44 +0530

    Add files via upload

---
+++ src/components/GallaryBody.jsx
@@ -0,0 +1,18 @@
+import "./GallaryBody.css"
+
+export default function GallaryBody(props){
+  const imageData = props.imageData;
+  return(
+    <div>
+      {
+        imageData.map(image=>{
+          return(
+            <div key={image.id} className="column">
+              <img src={image.img} alt=""></img>
+            </div>
+          )
+        })
+      }
+    </div>
+  )
+}
